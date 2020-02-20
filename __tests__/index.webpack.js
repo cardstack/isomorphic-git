dc6b94635f5012ccc6ebfdcc637c7253833a0bb8
Edward Faulkner <ef@alum.mit.edu>
2020-02-20 16:03:40 -0500

    building for local use

--- __tests__/index.webpack.js
+++
@@ -1,5 +0,0 @@
-// require all modules in the current directory, but not subdirectories, matching with "/test-"
-// @ts-ignore
-const testsContext = require.context('.', false, /\/test-.*$/)
-
-testsContext.keys().forEach(testsContext)
--- src/utils/symbols.js
+++
@@ -1,7 +0,0 @@
-// This is part of an elaborate system to facilitate code-splitting / tree-shaking.
-// commands/walk.js can depend on only this, and the actual Walker classes exported
-// can be opaque - only having a single property (this symbol) that is not enumerable,
-// and thus the constructor can be passed as an argument to walk while being "unusable"
-// outside of it.
-export const GitWalkBeta1Symbol = Symbol('GitWalkBeta1Symbol')
-export const GitWalkBeta2Symbol = Symbol('GitWalkBeta2Symbol')
